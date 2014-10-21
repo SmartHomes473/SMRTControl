9af499ae0d51e0911a52fc3dc9fd1e5e0b0178cb
Chris Fulara <fularac@umich.edu>
2014-10-21 16:00:58 +0000

    TX connects to WWF and Roomba now

--- TX.py
+++ TX.py
@@ -4,7 +4,7 @@ import sys
 
 ser = serial.Serial('/dev/ttyAMA0',9600)
 databases = { 1 : mdb.connect('localhost', 'root', 'smarthouse', 'wwfSample'),
-			  2 : mdb.connect('localhost', 'root', 'smarthouse', 'roomba');
+	      2 : mdb.connect('localhost', 'root', 'smarthouse', 'roomba')
 };
 
 for key in databases.keys() :
@@ -18,6 +18,6 @@ for key in databases.keys() :
 		ser.write(chr((row[1]>>8)&0xff)+chr(row[1]&0xff)) #Writes ExStatusLeng
 		ser.write(str(row[2])) #Writes ExtendedStatus
 		ser.write(chr(4))
-		cur.execute("UPDATE  `Communication` SET  `Status` = '0' WHERE  `Communication`.`Status` =1 LIMIT 1")
+		cur.execute("UPDATE  `Communication` SET  `Status` = '2' WHERE  `Communication`.`Status` =1 LIMIT 1")
 		db.commit()
 ser.close()
