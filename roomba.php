7e9b0bd24871ed54cbb98561057274c9583f0f9f
Ryan Wooster <rwooster@umich.edu>
2014-11-08 19:17:50 -0500

    new buttons working

--- roomba.php
+++ roomba.php
@@ -1,19 +1,3 @@
-<?php
-	function write_database($len, $data)
-	{
-		$db = new mysqli("localhost", "root", "smarthouse", "roomba");
-
-		if ($db->connect_errno > 0) {
-			die("Error connection to database: " . $db->connect_error);
-		}
-
-		$query = $db->prepare("UPDATE `Communication` SET `Status` = 1, `ExStatusLength` = ?, `ExtendedStatus` = ?");
-
-		$data_string = implode(array_map("strval", $data));
-		$query->bind_param("is", $len, $data_string);
-		$query->execute();
-	}
-?>
 
 <!DOCTYPE html>
 <html>
@@ -29,37 +13,18 @@
 	<script type="text/javascript">
 		function button_clicked(button)
 		{
-			$.post("roomba.php", button);
+			$.post("roomba_update_db.php", { command : button });
+			return false;
 		}
 	</script>
 
 
-	<a href="#0" onclick=button_clicked("clean"); class="button clean"></a>
-	<a href="#0" onclick=button_clicked("spot"); class="button spot"></a>
-	<a href="#0" onclick=button_clicked("max"); class="button max"></a>
-	<a href="#0" onclick=button_clicked("dock"); class="button dock"></a>
+	<a href="#0" onclick="button_clicked('default');" class="button clean"></a>
+	<a href="#0" onclick="button_clicked('spot');" class="button spot"></a>
+	<a href="#0" onclick="button_clicked('max');" class="button max"></a>
+	<a href="#0" onclick="button_clicked('dock');" class="button dock"></a>
 
 	
 
-	<?php
-		$commands = [
-			"init"		=> array(0x00),
-			"dock"		=> array(0x03),
-			"test"		=> array(0x05),
-			"default"	=> array(0x02, 0x00),
-			"spot"		=> array(0x02, 0x01),
-			"max"		=> array(0x02, 0x02)
-		];
-
-		$packet = array();
-		foreach($_POST as $key => $value)
-		{
-			if(isset($_POST[$key]))
-			{
-				$packet = $commands[$key];
-			}
-		}
-		write_database(count($packet), $packet);
-	?>
 </body>
 </html>
---
+++ roomba_update_db.php
@@ -0,0 +1,30 @@
+<?php
+	function write_database($len, $data)
+	{
+		$db = new mysqli("localhost", "root", "smarthouse", "roomba");
+
+		if ($db->connect_errno > 0) {
+			die("Error connection to database: " . $db->connect_error);
+		}
+
+		$query = $db->prepare("UPDATE `Communication` SET `Status` = 1, `ExStatusLength` = ?, `ExtendedStatus` = ?");
+
+		$data_string = implode(array_map("strval", $data));
+		$query->bind_param("is", $len, $data_string);
+		$query->execute();
+	}
+
+	$commands = [
+			"init"		=> array(0x00),
+			"dock"		=> array(0x03),
+			"test"		=> array(0x05),
+			"default"	=> array(0x02, 0x00),
+			"spot"		=> array(0x02, 0x01),
+			"max"		=> array(0x02, 0x02)
+		];
+
+	$packet = array();
+	$packet = $commands[$_POST["command"]];
+
+	write_database(count($packet), $packet);
+?>
