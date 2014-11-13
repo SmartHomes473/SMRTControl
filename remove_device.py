c3e4d52be20a0b686f05f1c54a6505e7cdca4644
unknown <rwooster@eecs373-09.adsroot.itcs.umich.edu>
2014-11-13 15:33:13 -0500

    added remove device script

---
+++ remove_device.py
@@ -0,0 +1,40 @@
+from subprocess import call
+import MySQLdb as mdb
+import sys
+import pdb
+
+def main():
+
+	device_to_remove = sys.argv[1]	
+
+	#find device line, get folder/database name and remove the line
+	folder = None
+	database = None
+
+	with open("devices.txt", "r") as device_file:
+		for line in device_file:
+			device_line = line.split(",")	
+
+			if device_line[1] == device_to_remove:
+				folder = device_line[0]
+				database = device_line[4].replace("\n", "")
+			else:
+				with open("devices_new.txt", "w") as newdevice_file:
+					newdevice_file.write(line)
+
+	call(["mv", "devices_new.txt", "devices.txt"])	
+
+	#drop database
+	db = mdb.connect("localhost", "root", "smarthouse", database)
+	cursor = db.cursor()
+	sql = "DROP DATABASE `XXX`;".replace("XXX", database)
+	cursor.execute(sql)
+	db.commit()
+
+	#delete folder
+	pdb.set_trace()
+	call(["rm", "-rf", folder])
+
+
+if __name__ == "__main__":
+	main()
