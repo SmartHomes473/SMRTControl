de5a111bbc01755a10b5fdc78cc4658b9a1c2b69
Ryan Wooster <rwooster@umich.edu>
2014-11-29 13:34:23 -0500

    update sql

--- temperature_control/temperature_control.sql
+++ temperature_control/temperature_control.sql
@@ -9,3 +9,13 @@ CREATE TABLE IF NOT EXISTS `Communication` (
 
 INSERT INTO `Communication` (`Status`, `ExStatusLength`, `ExtendedStatus`) VALUES
 (0, 0, '0');
+
+CREATE TABLE IF NOT EXISTS `current_setpoint` (
+  `temperature` float NOT NULL,
+  `units` enum('Kelvin','Celsius','Farenheit','') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+INSERT INTO `current_setpoint` (`temperature`, `units`) VALUES
+(30, 'Celsius');
+
+
